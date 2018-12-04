4197d777603a4e7e825de6ba0ecfd2ca3cfb068f
Aziz Saifuddin <azizs2@illinois.edu>
2018-12-03 23:04:26 -0600

    R.java error

--- app/src/main/java/com/example/azizsaifuddin/pokedex/Main2Activity.java
+++ app/src/main/java/com/example/azizsaifuddin/pokedex/Main2Activity.java
@@ -1,13 +1,100 @@
 package com.example.azizsaifuddin.pokedex;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import org.json.JSONObject;
 
 public class Main2Activity extends AppCompatActivity {
+    private static RequestQueue requestQueue;
+    private static final String TAG = "Main2Activity";
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestQueue = Volley.newRequestQueue(this);
         setContentView(R.layout.activity_main2);
+        url = "https://pokeapi.co/api/v2/pokemon/" + MainActivity.getInput() + "/";
+        Button stats = findViewById(R.id.statsevolution);
+        new DownloadImageTask((ImageView) findViewById(R.id.pokemonImage)) //pokemonimage
+                .execute("https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"); //pokemon image
+        stats.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+                startActivity(intent);
+            }
+        });
+        startAPICall();
+    }
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    url,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            TextView test = findViewById(R.id.nationaldex);
+                            test.setText("test");
+
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.w(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+            //progressBar.setVisibility(View.INVISIBLE);
+        } catch (Exception e) {
+            e.printStackTrace();
+            //display error in text box (pokemon not found).
+        }
+    }
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) { //initialize
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
     }
 }
--- app/src/main/java/com/example/azizsaifuddin/pokedex/MainActivity.java
+++ app/src/main/java/com/example/azizsaifuddin/pokedex/MainActivity.java
@@ -8,8 +8,14 @@ import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 import java.util.Scanner;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.android.volley.Request;
 import com.android.volley.Request;
@@ -24,65 +30,21 @@ public class MainActivity extends AppCompatActivity {
 
     private static RequestQueue requestQueue;
     private static final String TAG = "MainActivity";
+    private static String input = "chimchar";
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Set up a queue for our Volley requests
-        requestQueue = Volley.newRequestQueue(this);
-
-        // Load the main layout for our activity
         setContentView(R.layout.activity_main);
-
-        // Attach the handler to our UI button
-        final Button searchForPokemon = findViewById(R.id.search);
+        Button searchForPokemon = findViewById(R.id.button);
         searchForPokemon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                //Log.d(TAG, "Start API button clicked");
-                startAPICall();  //this method does the main things.
-                TextView test = findViewById(R.id.viewInfo);
-                //test.setText("HELLO WORLD!");
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
             }
         });
-
-        // Make sure that our progress bar isn't spinning and style it a bit
-        //ProgressBar progressBar = findViewById(R.id.progressBar);
-        //progressBar.setVisibility(View.INVISIBLE);
     }
-    void startAPICall() {
-        try {
-            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
-                    Request.Method.GET,
-                    "https://pokeapi.co/api/v2/pokemon/chimchar/",
-                    null,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(final JSONObject response) {
-                            //display pokemon name in text box.
-                            String raw = response.toString();
-                            TextView toview = findViewById(R.id.viewInfo);
-                            PokemonSearch attempt = new PokemonSearch(raw, "Dialga");
-                            toview.setText(attempt.hiddenPassive());
-                            //toview.setText(raw);
-                            Log.w(TAG, "Success");
-                            ProgressBar stop = findViewById(R.id.progressBar);
-                            stop.setVisibility(View.INVISIBLE);
-
-                        }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(final VolleyError error) {
-                    TextView toview = findViewById(R.id.viewInfo);
-                    //toview.setText("BIG ERROR");
-                    Log.w(TAG, error.toString());
-                }
-            });
-            requestQueue.add(jsonObjectRequest);
-            //progressBar.setVisibility(View.INVISIBLE);
-        } catch (Exception e) {
-            e.printStackTrace();
-            //display error in text box (pokemon not found).
-        }
+    public static String getInput() {
+        return input;
     }
 }
